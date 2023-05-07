352609bed4b619a83a5c948ac2370ffe139f1ed2
Thiago Costa <97390710+thiago-abc@users.noreply.github.com>
2023-05-07 17:26:25 -0300

    new codewars task

---
+++ grade_book.py
@@ -0,0 +1,27 @@
+"""
+Grade book
+
+Complete the function so that it finds the average of the three scores passed to it and returns the letter 
+value associated with that grade.
+Numerical Score 	Letter Grade
+90 <= score <= 100 	'A'
+80 <= score < 90 	'B'
+70 <= score < 80 	'C'
+60 <= score < 70 	'D'
+0 <= score < 60 	'F'
+
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+"""
+
+def get_grade(s1, s2, s3):
+	if (s1 + s2 + s3)/3 >= 90 and (s1 + s2 + s3)/3 <= 100:
+		return 'A'
+	elif (s1 + s2 + s3)/3 >= 80 and (s1 + s2 + s3)/3 < 90:
+		return 'B'
+	elif (s1 + s2 + s3)/3 >= 70 and (s1 + s2 + s3)/3 < 80:
+		return 'C'
+	elif (s1 + s2 + s3)/3 >= 60 and (s1 + s2 + s3)/3 < 70:
+		return 'D'
+	elif (s1 + s2 + s3)/3 >= 0 and (s1 + s2 + s3)/3 < 60:
+		return 'F'
+
