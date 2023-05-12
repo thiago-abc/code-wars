b578d8c58d118bfa0e5a15849633570f64992686
Thiago Costa <97390710+thiago-abc@users.noreply.github.com>
2023-05-12 09:51:57 -0300

    new codewars task

---
+++ shortest_word.py
@@ -0,0 +1,11 @@
+"""
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+"""
+
+def find_short(s):
+	array = s.split()
+	arr = sorted(array, key = len)
+	return len(arr[0])
+
