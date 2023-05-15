9dcc849608963d5285e56dd94528aa0194cea4ce
Thiago Costa <97390710+thiago-abc@users.noreply.github.com>
2023-05-15 07:51:51 -0300

    new codewars task

---
+++ convert_number_to_string.py
@@ -0,0 +1,15 @@
+"""
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+Examples (input --> output):
+
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+"""
+
+def number_to_string(num):
+	return str(num)
+
+print(type(number_to_string(123)))
