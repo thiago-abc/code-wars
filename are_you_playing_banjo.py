5b50a650dc0e647fa98f60328400df93b5d9367e
Thiago Costa <97390710+thiago-abc@users.noreply.github.com>
2023-05-22 16:08:40 -0300

    new codewars task

---
+++ are_you_playing_banjo.py
@@ -0,0 +1,16 @@
+"""
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+
+Names given are always valid strings.
+"""
+
+def are_you_playing_banjo(name):
+	return f'{name} plays banjo' if name[0] == 'r' or name[0] == 'R' else f'{name} does not play banjo'
+
+
