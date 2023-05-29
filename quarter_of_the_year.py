f97261fa62537f6df8048652505c8a377370e261
Thiago Costa <97390710+thiago-abc@users.noreply.github.com>
2023-05-29 13:48:06 -0300

    new codewars task

---
+++ quarter_of_the_year.py
@@ -0,0 +1,21 @@
+"""
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+    1 <= month <= 12
+"""
+
+def quarter_of(month):
+    if month >= 1 and month <= 3:
+        return 1
+    elif month >= 4 and month <= 6:
+        return 2
+    elif month >= 7 and month <= 9:
+        return 3
+    elif month >= 10 and month <= 12:
+        return 4
+
+print(quarter_of(12))
